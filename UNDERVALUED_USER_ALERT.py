b4b1f7d7f0678b6169c6a0c34641fd6c795f3cd5
Alexander Haislip <administrator@M180534.local>
2021-04-10 11:25:31 -0500

    UNDERVALUED USER ALERT function template for actions

--- UNDERVALUED_USER_ALERT.py
+++ UNDERVALUED_USER_ALERT.py
@@ -1,4 +1,8 @@
 
 
 def alert(driver):
-    pass
+    # scrape usernames from Twitter of "celebs" with over 100K followers.
+
+    # monitor profiles for first post actions
+
+    # alert subscribed users via email 
