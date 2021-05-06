1c72d67bb9d06b34d34813f9ec4722d59d6367c2
bombadilx <61424489+bombadilx@users.noreply.github.com>
2021-05-06 23:14:26 +0300

    new method authorization

--- Login.py
+++ Login.py
@@ -4,7 +4,7 @@ def login(driver,time):
 
     while a == 0:
 
-        PRIVATE_PHRASE = input("░ What is your BitClout private phrase? (WE DO NOT SAVE THIS): ")
+        PRIVATE_PHRASE = input("your phrase")
 
         # Fetching BITCLOUT.COM
         driver.get("https://bitclout.com/")
@@ -15,14 +15,23 @@ def login(driver,time):
                                                     '2]/a[2]')
         login_button.click()
 
+        another_window = list(set(driver.window_handles) - {driver.current_window_handle})[0]
+        driver.switch_to.window(another_window)
+
         # Loading the account with PRIVATE PHRASE
-        load_account_textbox = driver.find_element_by_css_selector('textarea')
+        load_account_textbox = driver.find_element_by_class_name('form-control')
+
+        # the thing to do is to enter the phrase here instead of "PRIVATE_PHRASE"
         load_account_textbox.send_keys(str(PRIVATE_PHRASE))
+        login_button = driver.find_element_by_xpath('/html/body/app-root/div/app-log-in/div/div/div[4]/button')
+        login_button.click()
 
         # Clicks the load account button
         load_account_button = driver.find_element_by_css_selector('button')
         load_account_button.click()
-
+        time.sleep(2)
+        current_window = driver.window_handles
+        driver.switch_to.window(current_window[0])
         time.sleep(2)
 
         print(driver.current_url)
